3047bd89b74f8f089f2f413685340bf3f4e45460
Marat Taktarov <zeldrizzyt@gmail.com>
2026-04-22 14:23:17 +0300

    scale MainVerticle to use all available event loops

--- src/main/java/com/webhookservice/App.java
+++ src/main/java/com/webhookservice/App.java
@@ -1,5 +1,6 @@
 package com.webhookservice;
 
+import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
 import org.slf4j.Logger;
@@ -10,13 +11,21 @@ public class App {
     private static final Logger log = LoggerFactory.getLogger(App.class);
 
     public static void main(String[] args) {
+        int cpus = Runtime.getRuntime().availableProcessors();
+
         VertxOptions options = new VertxOptions()
-                .setEventLoopPoolSize(Runtime.getRuntime().availableProcessors());
+                .setEventLoopPoolSize(cpus)
+                .setWorkerPoolSize(cpus * 2);
 
         Vertx vertx = Vertx.vertx(options);
 
-        vertx.deployVerticle(new MainVerticle())
-                .onSuccess(id -> log.info("MainVerticle deployed: {}", id))
+        int instances = cpus;
+        DeploymentOptions deploymentOptions = new DeploymentOptions()
+                .setInstances(instances)
+                .setHa(false);
+
+        vertx.deployVerticle("com.webhookservice.MainVerticle", deploymentOptions)
+                .onSuccess(id -> log.info("MainVerticle deployed: {} with {} instances", id, instances))
                 .onFailure(err -> {
                     log.error("Failed to deploy MainVerticle", err);
                     vertx.close().onComplete(v -> System.exit(1));
