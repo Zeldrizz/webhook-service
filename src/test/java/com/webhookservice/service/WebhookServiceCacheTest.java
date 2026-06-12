a40a5c7f5607d4b5ce8add13f01f64c65cc3f92d
Marat Taktarov <zeldrizzyt@gmail.com>
2026-06-12 19:29:15 +0300

    fix(test): remove unused import

--- src/test/java/com/webhookservice/service/WebhookServiceCacheTest.java
+++ src/test/java/com/webhookservice/service/WebhookServiceCacheTest.java
@@ -23,7 +23,6 @@ import java.util.function.UnaryOperator;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
